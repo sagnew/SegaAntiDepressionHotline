89ce9d9f15c70444171e34cc2a335384919d7f00
Sam Agnew <sagnew@eden.rutgers.edu>
2016-02-25 17:32:16 -0500

    Add more menu options

--- app.rb
+++ app.rb
@@ -8,17 +8,22 @@ songs = {
     '3' => 'http://gnarly.io/OutRunSplashWave.mp3',
     '4' => 'http://gnarly.io/PowerDriftLikeTheWind.mp3',
     '5' => 'http://gnarly.io/PhantasyStar2.mp3',
+    '6' => 'http://gnarly.io/Quartet.mp3',
 }
 
 get '/call' do
   twiml = Twilio::TwiML::Response.new do |r|
     r.Gather :numDigits => '1', :action => '/play', :method => 'get' do |g|
+      g.Say 'Welcome to the post MAGFest depression hotline.'
+      g.Say 'Where you can listen to happy SEGA tunes.'
       g.Say 'For Space Harrier press 1'
       g.Say 'For Sonic 2 press 2'
       g.Say 'For Outrun press 3'
       g.Say 'For Power Drift press 4'
       g.Say 'For Phantasy Star 2 press 5'
+      g.Say 'For Quartet Stage 1 press 6'
       g.Say 'Press any other key to start over.'
+      g.Say 'Courtesy of theshizz dot org and the MAG Underground staff'
     end
   end
   twiml.text
@@ -26,7 +31,11 @@ end
 
 get '/play' do
   twiml = Twilio::TwiML::Response.new do |r|
-    r.Play songs[params['Digits']]
+    if songs.has_key?(params['Digits'])
+      r.Play songs[params['Digits']]
+    else
+      r.Play songs['1']
+    end
   end
   twiml.text
 end
